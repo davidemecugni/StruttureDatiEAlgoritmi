f1912b6fd81f4ec7e7373b663759d5af4e8afd29
davidemecugni <299046@studenti.unimore.it>
2023-05-01 20:56:21 +0200

    Fixed pivot euristic description

--- ElemTypeEOrdinamento/VectorSort/vettore.c
+++ ElemTypeEOrdinamento/VectorSort/vettore.c
@@ -3,7 +3,7 @@ void QuickSort(Vector* v, int start, int stop) {
 	//Controllo per impedire la chiamata continua anche quando dim=1
 	if (start < stop) {
 		int sinistra = start, destra = stop;
-		//Il pivot viene arbitrariamente scelto
+		//Il pivot viene scelto come media dei due vettori(si presume che sia vicina alla mediana)
 		int pivot = (v->data[start] + v->data[stop]) / 2 ;
 		do {
 			while (v->data[sinistra] < pivot) sinistra++;
