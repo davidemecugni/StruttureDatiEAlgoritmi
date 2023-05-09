a97c4783f419409431c37dfccd154c65380bbc88
davidemecugni <299046@studenti.unimore.it>
2023-05-09 12:41:40 +0200

    Added comment

--- EsameDiLaboratorio15/CreaDaInterni/crea_da_interni.c
+++ EsameDiLaboratorio15/CreaDaInterni/crea_da_interni.c
@@ -5,6 +5,7 @@ typedef struct Item Item;
 Item* CreaDaInterni(const Item* i, double r) {
     Item* res = ListCreateEmpty();
     while (!ListIsEmpty(i)) {
+        //If the point is inside the radius
         if (sqrt((double)(i->value.x * i->value.x) + (double)(i->value.y * i->value.y)) < r) {
             res = ListInsertBack(res, &i->value);
         }
